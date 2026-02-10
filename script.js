b2e44db26b503bd20b105799acb8f8322bad732c
mikemyan <mikeyan1997@gmail.com>
2026-02-10 16:34:14 +0800

    new

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-  title.textContent = "Yippeeee!";
+  title.textContent = "Love you 老婆!";
 
   catImg.src = "cat_dance.gif";
 
